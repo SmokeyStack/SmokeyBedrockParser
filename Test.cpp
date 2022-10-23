30cd7ad4e62805f17212851e8b76c715830d3ede
SmokeyStack <seansyahril04@gmail.com>
2022-10-23 17:16:10 -0400

    New method to parse nbt

--- Test.cpp
+++ Test.cpp
@@ -18,113 +18,43 @@ static std::vector<char> ReadBytes(char const* filename) {
     return result;
 }
 
-std::string getTag(std::vector<char> test, int loc) {
-    switch (test[loc]) {
-        case 0x00:
-            return "TAG_End";
-        case 0x01:
-            return "TAG_Byte";
-        case 0x02:
-            return "TAG_Short";
-        case 0x03:
-            return "TAG_Int";
-        case 0x04:
-            return "TAG_Long";
-        case 0x05:
-            return "TAG_Float";
-        case 0x06:
-            return "TAG_Double";
-        case 0x07:
-            return "TAG_Byte_Array";
-        case 0x08:
-            return "TAG_String";
-        case 0x09:
-            return "TAG_List";
-        case 0x0A:
-            return "TAG_Compound";
-        case 0x0B:
-            return "TAG_Int_Array";
-        case 0x0C:
-            return "TAG_Long_Array";
-        default:
-            return "Not a tag!";
+std::string readTagName(std::vector<char> payload, int location, size_t size) {
+    std::string test{};
+    for (auto it = 0; it < size; it++) test += payload[location + 3 + it];
+    return test;
+};
+
+void readPayLoad(std::vector<char> payload, int location) {
+    if (payload[location] == 0x0a) {
+        int a = (int)payload[location + 2];
+        std::cout << readTagName(payload, location, a);
+        readPayLoad(payload, location + 3 + a);
+    }
+    if (payload[location] == 0x08) {
+        int a = (int)payload[location + 2];
+        std::cout << readTagName(payload, location, a);
+        int b = (int)payload[location + 4 + a];
+        std::cout << readTagName(payload, location + 6, b);
+        readPayLoad(payload, location + 5 + a + b);
     }
 }
-
-std::string hex_to_string(const std::string& s) {
-    std::string output;
-    size_t count = s.length() / 2;
-
-    for (size_t a = 0; a < count; a++) {
-        uint32_t b = 0;
-        std::stringstream ss;
-        ss << std::hex << s.substr(a * 2, 2);
-        ss >> b;
-        output.push_back(static_cast<unsigned char>(b));
-    }
-
-    return output;
-}
-
 int main(int argc, char* argv[]) {
-    int string_size{-1};
     int count{0};
-    int key_count{0};
     size_t length{ReadBytes(argv[1]).size()};
-    char nbt_bytes[2048]{};
-    std::string root_name{};
-    std::string value{};
-    std::array key_name = {"name", "value"};
+
+    std::vector<char> payload(ReadBytes(argv[1]).size());
 
     for (auto it = ReadBytes(argv[1]).begin(); it != ReadBytes(argv[1]).end();
          it++) {
-        nbt_bytes[count] = *it;
+        payload[count] = *it;
         count++;
     }
 
-    for (int a = 0; a < length; a++) printf("%02x", nbt_bytes[a]);
+    for (int a = 0; a < length; a++) printf("%02x", payload[a]);
 
-    std::cout << "\t|\tOriginal Bytes\t|\t" << count << "\n---\n";
+    std::cout << "\n-----\n";
 
-    if (ReadBytes(argv[1])[0] == 0x0a) {
-        char* pc = (char*)std::memchr(nbt_bytes, 0x00, sizeof nbt_bytes);
-
-        while ((pc - nbt_bytes + 3 + string_size) < count) {
-            if (key_count > 1) key_count = 0;
-            value = "";
-            for (int a = 0; a < length; a++) printf("%02x", nbt_bytes[a]);
-
-            pc = (char*)std::memchr(nbt_bytes, 0x00, sizeof nbt_bytes);
-            string_size = ReadBytes(argv[1])[pc - nbt_bytes + 1];
-
-            if (pc != nullptr)
-                printf("\t|\tFound at: %d\t|\tString Size: %d\n",
-                       pc - nbt_bytes + 1, string_size);
-
-            if ((pc - nbt_bytes + 1) == 2) {
-                for (size_t a = pc - nbt_bytes + 2;
-                     a < pc - nbt_bytes + 2 + string_size; a++)
-                    root_name += ReadBytes(argv[1])[a];
-
-                j["name"] = root_name;
-                std::string tag{getTag(ReadBytes(argv[1]),
-                                       pc - nbt_bytes + 2 + string_size)};
-                j["value"] = {{{"tag_type", tag}}};
-            } else {
-                for (size_t a = pc - nbt_bytes + 2;
-                     a < pc - nbt_bytes + 2 + string_size; a++)
-                    value += ReadBytes(argv[1])[a];
-                std::cout << "The value is: " << value << ".\n";
-                j["value"][0].push_back({key_name[key_count], value});
-                key_count++;
-            }
-
-            for (size_t a = 0; a < pc - nbt_bytes + string_size; a++)
-                nbt_bytes[a] = '\b';
-        }
-    }
-
-    std::cout << j.dump(4);
+    readPayLoad(payload, 0);
 
     // std::ifstream thingy(argv[1], std::ios::binary);
     // std::vector<char> bytes((std::istreambuf_iterator<char>(thingy)),
