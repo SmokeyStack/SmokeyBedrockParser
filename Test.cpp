14e097415bc009b3b486c1f8f6facbfa01cfbaed
SmokeyStack <seansyahril04@gmail.com>
2022-10-30 19:04:03 -0400

    Successfully parsed little-endian

--- Test.cpp
+++ Test.cpp
@@ -86,11 +86,18 @@ int8_t readByte(std::vector<char> payload, int location) {
  * @returns
  * Short value
  */
-int16_t readShort(std::vector<char> payload, int location) {
+int16_t readShort(std::vector<char> payload, int location, bool is_little) {
     union {
         char c[2];
         int16_t s;
     } u;
+
+    if (is_little) {
+        u.c[0] = payload[location];
+        u.c[1] = payload[location + 1];
+
+        return u.s;
+    }
 
     u.c[1] = payload[location];
     u.c[0] = payload[location + 1];
@@ -110,7 +117,7 @@ int16_t readShort(std::vector<char> payload, int location) {
  * @returns
  * Int value
  */
-int32_t readInt(std::vector<char> payload, int location) {
+int32_t readInt(std::vector<char> payload, int location, bool is_little) {
     union {
         char c[4];
         int32_t i;
@@ -118,7 +125,15 @@ int32_t readInt(std::vector<char> payload, int location) {
 
     int count{0};
 
-    for (int a = 3; a > 0; a--) {
+    if (is_little) {
+        for (int a = 0; a < 3; a++) {
+            u.c[a] = payload[location + count];
+            count++;
+        }
+        return u.i;
+    }
+
+    for (int a = 3; a > -1; a--) {
         u.c[a] = payload[location + count];
         count++;
     }
@@ -174,13 +189,21 @@ int64_t readLong(std::vector<char> payload, int location) {
  * @returns
  * Float value
  */
-float readFloat(std::vector<char> payload, int location) {
+float readFloat(std::vector<char> payload, int location, bool is_little) {
     union {
         char c[4];
         float f;
     } u;
 
     int count{0};
+
+    if (is_little) {
+        for (int a = 0; a < 4; a++) {
+            u.c[a] = payload[location + count];
+            count++;
+        }
+        return u.f;
+    }
 
     for (int a = 3; a > 0; a--) {
         u.c[a] = payload[location + count];
@@ -202,13 +225,21 @@ float readFloat(std::vector<char> payload, int location) {
  * @returns
  * Double value
  */
-double readDouble(std::vector<char> payload, int location) {
+double readDouble(std::vector<char> payload, int location, bool is_little) {
     union {
         char c[8];
         double d;
     } u;
 
     int count{0};
+
+    if (is_little) {
+        for (int a = 0; a < 8; a++) {
+            u.c[a] = payload[location + count];
+            count++;
+        }
+        return u.d;
+    }
 
     for (int a = 7; a > 0; a--) {
         u.c[a] = payload[location + count];
@@ -227,64 +258,66 @@ double readDouble(std::vector<char> payload, int location) {
  * @param location
  * Location of the payload to start reading
  */
-void readPayLoad(std::vector<char> payload, int location) {
+void readPayLoad(std::vector<char> payload, int location, int endian) {
     printf("\n-----\n 0x%02x - ", payload[location]);
+    bool is_little = false;
+    if (endian == 1) is_little = true;
 
     if (payload[location] == 0x00 && location <= payload.size())
-        readPayLoad(payload, location + 1);
+        readPayLoad(payload, location + 1, endian);
 
     if (payload[location] == 0x01) {
-        int a = (int)payload[location + 2];
+        int a = (int)payload[location + endian];
         std::cout << readTagName(payload, location, a);
         printf(": %d", readByte(payload, location + 3 + a));
-        readPayLoad(payload, location + 4 + a);
+        readPayLoad(payload, location + 4 + a, endian);
     }
 
     if (payload[location] == 0x02) {
-        int a = (int)payload[location + 2];
+        int a = (int)payload[location + endian];
         std::cout << readTagName(payload, location, a);
-        std::cout << ": " << readShort(payload, location + 3 + a);
-        readPayLoad(payload, location + 5 + a);
+        std::cout << ": " << readShort(payload, location + 3 + a, is_little);
+        readPayLoad(payload, location + 5 + a, endian);
     }
 
     if (payload[location] == 0x03) {
-        int a = (int)payload[location + 2];
+        int a = (int)payload[location + endian];
         std::cout << readTagName(payload, location, a);
-        printf(": %d", readInt(payload, location + 3 + a));
-        readPayLoad(payload, location + 7 + a);
+        printf(": %d", readInt(payload, location + 3 + a, is_little));
+        readPayLoad(payload, location + 7 + a, endian);
     }
 
     if (payload[location] == 0x04) {
-        int a = (int)payload[location + 2];
+        int a = (int)payload[location + endian];
         std::cout << readTagName(payload, location, a);
         std::cout << ": " << readLong(payload, location + 3 + a);
-        readPayLoad(payload, location + 11 + a);
+        readPayLoad(payload, location + 11 + a, endian);
     }
 
     if (payload[location] == 0x05) {
-        int a = (int)payload[location + 2];
+        int a = (int)payload[location + endian];
         std::cout << readTagName(payload, location, a);
-        printf(": %f", readFloat(payload, location + 3 + a));
-        readPayLoad(payload, location + 7 + a);
+        printf(": %f", readFloat(payload, location + 3 + a, is_little));
+        readPayLoad(payload, location + 7 + a, endian);
     }
 
     if (payload[location] == 0x06) {
-        int a = (int)payload[location + 2];
+        int a = (int)payload[location + endian];
         std::cout << readTagName(payload, location, a);
-        std::cout << ": " << readDouble(payload, location + 3 + a);
-        readPayLoad(payload, location + 11 + a);
+        std::cout << ": " << readDouble(payload, location + 3 + a, is_little);
+        readPayLoad(payload, location + 11 + a, endian);
     }
 
     if (payload[location] == 0x08) {
-        int a = (int)payload[location + 2];
+        int a = (int)payload[location + endian];
         std::cout << readTagName(payload, location, a);
-        int b = (int)payload[location + 4 + a];
+        int b = (int)payload[location + 2 + endian + a];
         std::cout << ": " << readTagName(payload, location + a + 2, b);
-        readPayLoad(payload, location + 5 + a + b);
+        readPayLoad(payload, location + 5 + a + b, endian);
     }
 
     if (payload[location] == 0x09) {
-        int a = (int)payload[location + 2];
+        int a = (int)payload[location + endian];
         std::cout << readTagName(payload, location, a) << "\n";
         int count{0};
         int b{0};
@@ -294,9 +327,10 @@ void readPayLoad(std::vector<char> payload, int location) {
             case 0x05:
                 for (count; count < (int)payload[location + 7 + a]; count++)
                     printf("%f\n",
-                           readFloat(payload, location + 8 + a + (count * 4)));
+                           readFloat(payload, location + 8 + a + (count * 4),
+                                     is_little));
 
-                readPayLoad(payload, location + 8 + a + (count * 4));
+                readPayLoad(payload, location + 8 + a + (count * 4), endian);
 
                 break;
             case 0x08:
@@ -308,7 +342,7 @@ void readPayLoad(std::vector<char> payload, int location) {
                     loc = loc + 2 + b;
                 }
 
-                readPayLoad(payload, location + 8 + a + loc);
+                readPayLoad(payload, location + 8 + a + loc, endian);
 
                 break;
             default:
@@ -317,13 +351,15 @@ void readPayLoad(std::vector<char> payload, int location) {
     }
 
     if (payload[location] == 0x0a) {
-        int a = (int)payload[location + 2];
+        int a = (int)payload[location + endian];
         std::cout << readTagName(payload, location, a);
-        readPayLoad(payload, location + 3 + a);
+        readPayLoad(payload, location + 3 + a, endian);
     }
 }
 
 int main(int argc, char* argv[]) {
+    if (argc < 3) return EXIT_FAILURE;
+
     int count{0};
     size_t length{ReadBytes(argv[1]).size()};
     std::vector<char> payload(ReadBytes(argv[1]).size());
@@ -338,28 +374,27 @@ int main(int argc, char* argv[]) {
 
     std::cout << "\n-----\n";
 
-    readPayLoad(payload, 0);
+    if (strcmp("--big", argv[2]) == 0) readPayLoad(payload, 0, 2);
+    if (strcmp("--little", argv[2]) == 0) readPayLoad(payload, 0, 1);
 
-    // std::vector<char> test = {
-    //     0x0a, 0x00, 0x00, 0x08, 0x04, 0x00, 0x6e, 0x61, 0x6d, 0x65, 0x0d,
-    //     0x00, 0x6d, 0x69, 0x6e, 0x65, 0x63, 0x72, 0x61, 0x66, 0x74, 0x3a,
-    //     0x61, 0x69, 0x72, 0x0a, 0x06, 0x00, 0x73, 0x74, 0x61, 0x74, 0x65,
-    //     0x73, 0x00, 0x03, 0x07, 0x00, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f,
-    //     0x6e, 0x01, 0x0a, 0x12, 0x01, 0x00, 0x0a, 0x00, 0x00, 0x08, 0x04,
-    //     0x00, 0x6e, 0x61, 0x6d, 0x65, 0x0f, 0x00, 0x6d, 0x69, 0x6e, 0x65,
-    //     0x63, 0x72, 0x61, 0x66, 0x74, 0x3a, 0x73, 0x74, 0x6f, 0x6e, 0x65,
-    //     0x0a, 0x06, 0x00, 0x73, 0x74, 0x61, 0x74, 0x65, 0x73, 0x08, 0x0a,
-    //     0x00, 0x73, 0x74, 0x6f, 0x6e, 0x65, 0x5f, 0x74, 0x79, 0x70, 0x65,
-    //     0x05, 0x00, 0x73, 0x74, 0x6f, 0x6e, 0x65, 0x00, 0x03, 0x07, 0x00,
-    //     0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x01, 0x0a, 0x12, 0x01,
-    //     0x00};
+    std::cout << "\n-----\n";
 
-    // readPayLoad(test, 0);
+    std::vector<char> test = {
+        0x0a, 0x00, 0x00, 0x08, 0x04, 0x00, 0x6e, 0x61, 0x6d, 0x65, 0x0d, 0x00,
+        0x6d, 0x69, 0x6e, 0x65, 0x63, 0x72, 0x61, 0x66, 0x74, 0x3a, 0x61, 0x69,
+        0x72, 0x0a, 0x06, 0x00, 0x73, 0x74, 0x61, 0x74, 0x65, 0x73, 0x00, 0x03,
+        0x07, 0x00, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x01, 0x0a, 0x12,
+        0x01, 0x00, 0x0a, 0x00, 0x00, 0x08, 0x04, 0x00, 0x6e, 0x61, 0x6d, 0x65,
+        0x0f, 0x00, 0x6d, 0x69, 0x6e, 0x65, 0x63, 0x72, 0x61, 0x66, 0x74, 0x3a,
+        0x73, 0x74, 0x6f, 0x6e, 0x65, 0x0a, 0x06, 0x00, 0x73, 0x74, 0x61, 0x74,
+        0x65, 0x73, 0x08, 0x0a, 0x00, 0x73, 0x74, 0x6f, 0x6e, 0x65, 0x5f, 0x74,
+        0x79, 0x70, 0x65, 0x05, 0x00, 0x73, 0x74, 0x6f, 0x6e, 0x65, 0x00, 0x03,
+        0x07, 0x00, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x01, 0x0a, 0x12,
+        0x01, 0x00};
+
+    readPayLoad(test, 0, 1);
 
     return 0;
 }
 
 // 0x0a,0x00,0x00,0x08,0x04,0x00,0x6e,0x61,0x6d,0x65,0x0d,0x00,0x6d,0x69,0x6e,0x65,0x63,0x72,0x61,0x66,0x74,0x3a,0x61,0x69,0x72,0x0a,0x06,0x00,0x73,0x74,0x61,0x74,0x65,0x73,0x00,0x03,0x07,0x00,0x76,0x65,0x72,0x73,0x69,0x6f,0x6e,0x01,0x0a,0x12,0x01,0x00,0x0a,0x00,0x00,0x08,0x04,0x00,0x6e,0x61,0x6d,0x65,0x0f,0x00,0x6d,0x69,0x6e,0x65,0x63,0x72,0x61,0x66,0x74,0x3a,0x73,0x74,0x6f,0x6e,0x65,0x0a,0x06,0x00,0x73,0x74,0x61,0x74,0x65,0x73,0x08,0x0a,0x00,0x73,0x74,0x6f,0x6e,0x65,0x5f,0x74,0x79,0x70,0x65,0x05,0x00,0x73,0x74,0x6f,0x6e,0x65,0x00,0x03,0x07,0x00,0x76,0x65,0x72,0x73,0x69,0x6f,0x6e,0x01,0x0a,0x12,0x01,0x00
-
-// 0a0000010004627974650002000573686f72740001030003696e7400000002050005666c6f617440400000060006646f75626c654010000000000000080006737472696e67000754657374696e670b0009696e745f6172726179000000030000002d000000050000000607000a627974655f6172726179000000030403020c000a6c6f6e675f6172726179000000030000000000000005000000000000000600000000000000070900046c69737403000000010000000409000c6175746f6c6973745f696e74030000000500000005000000060000000700007620fffffffffffffffffffffffeffffffac0900116175746f6c6973745f636f6d706f756e640a000000010800046e616d6500034142430600066865616c7468400c0000000000000000
-// 0a0000010400627974650002050073686f72740100030300696e7402000000050500666c6f617400004040060600646f75626c650000000000001040080600737472696e67070054657374696e670b0900696e745f6172726179030000002d0000000500000006000000070a00627974655f6172726179030000000403020c0a006c6f6e675f6172726179030000000500000000000000060000000000000007000000000000000904006c697374030100000004000000090c006175746f6c6973745f696e74030500000005000000060000000700000020760000ffffffacfffffffeffffffffffffffff0911006175746f6c6973745f636f6d706f756e640a010000000804006e616d6503004142430606006865616c74680000000000000c400000
