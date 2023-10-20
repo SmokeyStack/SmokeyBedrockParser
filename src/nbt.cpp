d0067adcdbe6c52c32e20c9e958c5e5457db687f
SmokeyStack <seansyahril04@gmail.com>
2023-10-19 22:51:58 -0400

    Successfully parsed level.dat

--- src/nbt.cpp
+++ src/nbt.cpp
@@ -2,6 +2,7 @@
 
 #include "nbt.h"
 #include "logger.h"
+#include "json.hpp"
 
 std::string makeIndent(int32_t indent, const char* hdr) {
 	std::string s;
@@ -12,69 +13,100 @@ std::string makeIndent(int32_t indent, const char* hdr) {
 	return s;
 }
 
+typedef struct NbtJson {
+	std::string name;
+	nlohmann::json nbt;
+};
+
 namespace smokey_bedrock_parser {
 	int32_t global_nbt_list_number = 0;
 	int32_t global_nbt_compound_number = 0;
 
-	int32_t ParseNbtTag(const char* header, int& indent, const NbtTag& tag) {
-		log::info("{}NBT Tag: {}", makeIndent(indent, header), tag.first);
+	nlohmann::json::object_t ParseNbtTag(const char* header, int& indent, const NbtTag& tag) {
+		log::trace("{}NBT Tag: {}", makeIndent(indent, header), tag.first);
+
+		nlohmann::json::object_t json;
 
 		nbt::tag_type nbt_type = tag.second->get_type();
 
 		switch (nbt_type) {
 		case nbt::tag_type::End:
-			log::info("TAG_END");
+			log::trace("TAG_END");
 			break;
 		case nbt::tag_type::Byte: {
 			nbt::tag_byte value = tag.second->as<nbt::tag_byte>();
-			log::info("TAG_BYTE: {}", value.get());
+			log::trace("TAG_BYTE: {}", value.get());
+			json[tag.first] = value.get();
 		}
 								break;
 		case nbt::tag_type::Short: {
 			nbt::tag_short value = tag.second->as<nbt::tag_short>();
-			log::info("TAG_SHORT: {}", value.get());
+			log::trace("TAG_SHORT: {}", value.get());
+			json[tag.first] = value.get();
 		}
 								 break;
 		case nbt::tag_type::Int: {
 			nbt::tag_int value = tag.second->as<nbt::tag_int>();
-			log::info("TAG_INT: {}", value.get());
+			log::trace("TAG_INT: {}", value.get());
+			json[tag.first] = value.get();
 		}
 							   break;
 		case nbt::tag_type::Long: {
 			nbt::tag_long value = tag.second->as<nbt::tag_long>();
-			log::info("TAG_LONG: {}", value.get());
+			log::trace("TAG_LONG: {}", value.get());
+			json[tag.first] = value.get();
 		}
 								break;
 		case nbt::tag_type::Float: {
 			nbt::tag_float value = tag.second->as<nbt::tag_float>();
-			log::info("TAG_FLOAT: {}", value.get());
+			log::trace("TAG_FLOAT: {}", value.get());
+			json[tag.first] = value.get();
 		}
 								 break;
 		case nbt::tag_type::Double: {
 			nbt::tag_double value = tag.second->as<nbt::tag_double>();
-			log::info("TAG_DOUBLE: {}", value.get());
+			log::trace("TAG_DOUBLE: {}", value.get());
+			json[tag.first] = value.get();
 		}
 								  break;
 		case nbt::tag_type::Byte_Array:
 			break;
 		case nbt::tag_type::String: {
 			nbt::tag_string value = tag.second->as<nbt::tag_string>();
-			log::info("TAG_STRING: {}", value.get());
+			log::trace("TAG_STRING: {}", value.get());
+			json[tag.first] = value.get();
 		}
 								  break;
-		case nbt::tag_type::List:
-			break;
+		case nbt::tag_type::List: {
+			nbt::tag_list value = tag.second->as<nbt::tag_list>();
+			int32_t list_number = global_nbt_compound_number++;
+			log::trace("LIST-{} {{", list_number);
+			indent++;
+
+			for (const auto& nbt_tag : value) {
+				json[tag.first].update(ParseNbtTag(header, indent, std::make_pair(std::string(""), nbt_tag.get().clone())));
+			}
+
+			if (--indent < 0)
+				indent = 0;
+
+			log::trace("{}}} LIST-{}", makeIndent(indent, header), list_number);
+		}
+								break;
 		case nbt::tag_type::Compound: {
 			nbt::tag_compound value = tag.second->as<nbt::tag_compound>();
 			int32_t compound_number = global_nbt_compound_number++;
-			log::info("TAG_COMPOUND: {} ({} tags)", compound_number, value.size());
+			log::trace("TAG_COMPOUND: {} ({} tags)", compound_number, value.size());
 			indent++;
+
 			for (const auto& nbt_tag : value) {
-				ParseNbtTag(header, indent, std::make_pair(nbt_tag.first, nbt_tag.second.get().clone()));
+				json[tag.first].update(ParseNbtTag(header, indent, std::make_pair(nbt_tag.first, nbt_tag.second.get().clone())));
 			}
+
 			if (indent-- < 0)
 				indent = 0;
-			log::info("{}}} COMPOUND-{}", makeIndent(indent, header), compound_number);
+
+			log::trace("{}}} COMPOUND-{}", makeIndent(indent, header), compound_number);
 		}
 									break;
 		case nbt::tag_type::Int_Array:
@@ -84,24 +116,22 @@ namespace smokey_bedrock_parser {
 		default:
 			break;
 		}
+
+		return json;
 	}
 
 	int32_t ParseNbt(const char* header, const char* buffer, int32_t buffer_length, NbtTagList& tag_list) {
 		int32_t indent = 0;
-
-		log::info("{}NBT Decode Start", makeIndent(indent, header));
-
+		log::trace("{}NBT Decode Start", makeIndent(indent, header));
 		global_nbt_list_number = 0;
 		global_nbt_compound_number = 0;
-
 		std::istringstream iss(std::string(buffer, buffer_length));
 		nbt::io::stream_reader reader(iss, endian::little);
-
 		tag_list.clear();
-
 		NbtTag tag;
 		bool done = false;
 		std::istream& stream = reader.get_istr();
+
 		while (!done && stream && !stream.eof()) {
 			try
 			{
@@ -111,14 +141,20 @@ namespace smokey_bedrock_parser {
 			{
 				if (!stream.eof())
 					log::error("Failed to read tag");
+
 				done = true;
 			}
 		}
 
+		NbtJson nbt_json;
+		nbt_json.name = "nbt";
+
 		for (const auto& nbt_tag : tag_list) {
-			ParseNbtTag(header, indent, nbt_tag);
+			nbt_json.nbt = ParseNbtTag(header, indent, nbt_tag);
 		}
-		log::info("{}NBT Decode End ({} tags)", makeIndent(indent, header), tag_list.size());
+
+		log::trace("{}NBT Decode End ({} tags)", makeIndent(indent, header), tag_list.size());
+		log::info("{}", nbt_json.nbt.dump(4));
 
 		return 0;
 	}
