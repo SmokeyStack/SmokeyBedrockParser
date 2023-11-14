71e89d93112048efca5d110a02bd8471fc0ae525
SmokeyStack <seansyahril04@gmail.com>
2023-11-14 14:50:31 -0500

    Got a square rendered

--- src/SmokeyBedrockParser.cpp
+++ src/SmokeyBedrockParser.cpp
@@ -1,7 +1,11 @@
 #include <cstdint>
 #include <cstdio>
 #include <filesystem>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/fwd.hpp>
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
@@ -20,62 +24,30 @@ static void GLFWErrorCallback(int error, const char* description) {
 	smokey_bedrock_parser::log::error("GLFW Error {}: {}", error, description);
 }
 
-int main(int argc, char** argv) {
-	using namespace smokey_bedrock_parser;
+const char* vertexShaderSource = R"(
+#version 460 core
+layout (location = 0) in vec4 aColor;
+layout (location = 1) in vec3 aPosition;
 
-	SetupLoggerStage1();
+out vec4 fColor;
 
-	auto console_log_level = Level::Info;
-	auto file_log_level = Level::Trace;
-	std::filesystem::path log_directory = "logs/debug.log";
+void main()
+{
+    fColor = aColor;
+    gl_Position = vec4(aPosition, 1.0);
+})";
+const char* fragmentShaderSource = R"(
+#version 460 core
+out vec4 FragColor;
 
-	SetupLoggerStage2(log_directory, console_log_level, file_log_level);
+in vec4 fColor;
 
-	world = std::make_unique<MinecraftWorldLevelDB>();
+void main()
+{
+    FragColor = fColor;
+})";
 
-	nfdchar_t* selected_folder = NULL;
-	std::string world_path = "";
-	static bool show_app_property_editor = false;
-	static bool open_file_dialog = false;
-
-	LoadJson("data/blocks.json");
-
-	glfwSetErrorCallback(GLFWErrorCallback);
-
-	if (!glfwInit())
-		return 1;
-
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-	// GL ES 2.0 + GLSL 100
-	const char* glsl_version = "#version 100";
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(__APPLE__)
-	// GL 3.2 + GLSL 150
-	const char* glsl_version = "#version 150";
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
-#else
-	// GL 3.0 + GLSL 130
-	const char* glsl_version = "#version 130";
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-#endif
-
-	GLFWwindow* window = glfwCreateWindow(1080, 720, "SmokeyBedrockParser", nullptr, nullptr);
-
-	if (window == nullptr)
-		return 1;
-
-	glfwMakeContextCurrent(window);
-
-	glfwSwapInterval(1); // Enable vsync
-
+void SetupImGuiConfigs() {
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
@@ -144,17 +116,168 @@ int main(int argc, char** argv) {
 		style.WindowRounding = 0.0f;
 		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
 	}
+}
 
+struct Vertex {
+	glm::vec3 position;
+	glm::vec4 color;
+};
+
+int main(int argc, char** argv) {
+	using namespace smokey_bedrock_parser;
+
+	SetupLoggerStage1();
+
+	auto console_log_level = Level::Info;
+	auto file_log_level = Level::Trace;
+	std::filesystem::path log_directory = "logs/debug.log";
+
+	SetupLoggerStage2(log_directory, console_log_level, file_log_level);
+
+	world = std::make_unique<MinecraftWorldLevelDB>();
+
+	nfdchar_t* selected_folder = NULL;
+	std::string world_path = "";
+	static bool show_app_property_editor = false;
+	static bool open_file_dialog = false;
+
+	LoadJson("data/blocks.json");
+	glfwSetErrorCallback(GLFWErrorCallback);
+
+	if (!glfwInit()) return 1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1080, 720, "SmokeyBedrockParser", nullptr, nullptr);
+
+	if (window == nullptr) {
+		glfwTerminate();
+
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		log::error("Failed to initialize GLAD");
+
+		return 1;
+	}
+
+	glViewport(0, 0, 720, 720);
+
+	glfwSwapInterval(1); // Enable vsync
+
+	/*
+	* Shader stuff
+	*/
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		log::error("ERROR::SHADER::VERTEX::COMPILATION_FAILED");
+		log::error("{}", infoLog);
+	}
+
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		log::error("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED");
+		log::error("{}", infoLog);
+	}
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		log::error("ERROR::SHADER::PROGRAM::LINKING_FAILED");
+		log::error("{}", infoLog);
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	Vertex triangle[4]{
+		{glm::vec3(0.5f, 0.5f, 0.0f), glm::vec4(1,1,1,1)},
+		{glm::vec3(0.5f, -0.5f, 0.0f), glm::vec4(1,1,1,1)},
+		{glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec4(1,1,1,1)},
+		{glm::vec3(-0.5f,  0.5f, 0.0f), glm::vec4(1,1,1,1)}
+
+	};
+
+	uint32_t indices[] = {  // note that we start from 0!
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+	};
+
+	uint32_t VAO, VBO, EBO;
+	glCreateVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	//SetupImGuiConfigs();
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+	// GL ES 2.0 + GLSL 100
+	const char* glsl_version = "#version 100";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+	// GL 3.2 + GLSL 150
+	const char* glsl_version = "#version 150";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+	// GL 3.0 + GLSL 130
+	const char* glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+#if false
 	// Setup Platform/Renderer backends
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init(glsl_version);
-	bool show_demo_window = true;
-	bool show_another_window = false;
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+#endif
 
 	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
-
+#if false
 		// Start the Dear ImGui frame
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
@@ -279,21 +402,24 @@ int main(int argc, char** argv) {
 		// Rendering
 		ImGui::Render();
 
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(0.5, 0.5, 0.5, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+#endif
 
+		glClearColor(0.5, 0.5, 0.5, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
-	}
+		glfwPollEvents();
+}
 
+#if false
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
+#endif
 
-	glfwDestroyWindow(window);
 	glfwTerminate();
 
 	return 0;
